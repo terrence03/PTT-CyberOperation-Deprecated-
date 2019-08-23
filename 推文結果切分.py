8e5675f07a7a8649446174e23bcd051a156790dd
terrence03 <terrence0303@protonmail.com>
2019-08-24 03:50:03 +0800

    測試完成

--- 推文結果切分.py
+++ 推文結果切分.py
@@ -1,6 +1,7 @@
 import csv
 import codecs
 import sqlite3
+import re
 from tqdm import tqdm
 
 """
@@ -9,9 +10,11 @@ from tqdm import tqdm
 並將資料存入sqlite中
 """
 
+#candidate_list = ["一般縣市候選人", "丁守中", "侯友宜", "其他直轄市候選人", "姚文智", "林佳龍", "柯文哲", "盧秀燕", "蘇貞昌", "鄭文燦", "陳其邁", "陳學聖", "韓國瑜", "高思博", "黃偉哲"]
+
 def main():
 
-    with open(r'D:\\research\\資料\\推文資料\\推文資料_柯文哲.csv', 'r', encoding="utf-8") as csvFile:
+    with open(r'D:\\research\\資料\\推文資料\\推文資料_侯友宜.csv', 'r', encoding="utf-8") as csvFile:
     
         rows = list(csv.reader(csvFile))
 
@@ -24,7 +27,7 @@ def main():
         for row in tqdm(rows):
             string = str(row)
             step1 = string.split("|")
-            step2 = step1[1].split(": ",1)
+            step2 = step1[1].split(":",1)
             step3 = step2[0].split(" ", 1)
 
             candidate = step1[0].replace("['", '')
@@ -35,22 +38,26 @@ def main():
             
             push_author = step3[1]
             
-            if(step2[1]).find("[00-12]:[00-60]") > 0 :
-                time = step2[1][-5:]
+            # 此處使用時間的正規表示法(\d{2}):(\d{2})
+            if re.search(r"(\d{2}):(\d{2})", step2[1]) != None:
+                time = re.search(r"(\d{2}):(\d{2})", step2[1]).group()
             else:
                 time = ""
             
-            if(step2[1]).find("[01-12]/[01-31]") > 0 :
-                date = step2[1][-11:-6]
+            # 此處使用日期的正規表示法(\d{2})/(\d{2})
+            if re.search(r"(\d{2})/(\d{2})", step2[1]) != None:
+                date = re.search(r"(\d{2})/(\d{2})", step2[1]).group()
             else:
                 date = ""
            
-            if(step2[1]).find("[000-999].[000-999].[000-999].[000-999]") > 0 :
-                ip = step2[1][-28:-12]
+            # 此處使用IP的正規表示法(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])
+            if re.search(r'(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])', step2[1]) != None:
+                ip = re.search(r'(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])', step2[1]).group()
             else:
                 ip = ""
 
-            push_comment = step2[1][0:-27].replace("', '", ',')
+            push_comment = re.sub(
+                "(\d{2}:\d{2})|(\d{2}/\d{2})|(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])", "", step2[1]).lstrip()  # 用re.sub取代時可以用|號來進行多個取代
 
             #print(candidate, postID)
 
@@ -58,7 +65,7 @@ def main():
             #包含IP資訊
             result_value = [postID, candidate, push_type, push_author, push_comment, ip, date, time]
 
-            c.execute("INSERT INTO 推文資料_含IP (文章ID,候選人,推文類型,推文作者,推文內文,推文IP,推文日期,推文時間) VALUES (?,?,?,?,?,?,?,?)", result_value)
+            c.execute("INSERT INTO 推文資料 (文章ID,候選人,推文類型,推文作者,推文內文,推文IP,推文日期,推文時間) VALUES (?,?,?,?,?,?,?,?)", result_value)
             
             conn.commit()
             
