8eaff5d97fee5734d1fa9f3b2e9affacb151a4a6
terrence03 <terrence0303@protonmail.com>
2019-09-07 19:23:32 +0800

    測試

--- 淨推數計算.py
+++ 淨推數計算.py
@@ -3,5 +3,11 @@ import pandas as pd
 from tqdm import tqdm
 
 #讀入資料
-conn = sqlite3.connect(r"D:\\研究\\資料\\data.db")
-print("Open database successfully")
+with sqlite3.connect(r'D:\\research\\推文資料.sqlite') as con:
+# read_sql_query和read_sql都能通過SQL語句從資料庫檔案中獲取資料資訊
+    data = pd.read_sql_query("SELECT * FROM 推文資料", con = con)
+# df = pd.read_sql("SELECT * FROM test_table", con=con)
+
+#print(data.shape)
+#print(data.dtypes)
+#print(data.head())
