f7cf1c3b83d8f6acbb3d32d991e8f25558815fc8
terrence03 <terrence0303@protonmail.com>
2019-09-25 08:17:59 +0800

    修改完成

--- 網軍資料處理.py
+++ 網軍資料處理.py
@@ -3,7 +3,7 @@ import sqlite3
 import pandas as pd
 
 with sqlite3.connect(r"D:\\research\\推文資料.sqlite") as con:
-    df = pd.read_sql("SELECT * FROM 網軍測試", con = con)
+    df = pd.read_sql("SELECT * FROM 結合資料_新", con = con)
 
 data = df.drop_duplicates()    #刪除整個row重複
 
@@ -17,17 +17,26 @@ conn.commit()
 conn.close()
 
 #%%
-grouped = data.groupby(['作者','推文作者'])
-grouped_cnt = grouped['推文內文']
 #計算某帳號在某作者文章下留言的次數
-print(grouped_cnt.agg('count'))
+grouped_1 = data.groupby(['作者','推文作者'], as_index = False)['推文內文']
 
 #%%
-'''
-grouped_upv = data.groupby(['作者', '推文作者', '推文類型' == '推'])
-grouped_upv_cnt = grouped['推文內文']
-#計算某帳號在某作者文章下推的次數
-#未完成，先擱置
-'''
+#計算留言帳號在每個作者下留言的機率
+grouped_2 = data.groupby(['推文作者','作者'], as_index = False)['推文內文']
+com = grouped_2.agg({'cnt': 'count'})
+
+sum_com = com.groupby(['推文作者'], as_index = False)['cnt'].agg({'sum':'sum'})
+cross_com = pd.merge(com, sum_com)
+cross_com['留言機率'] = cross_com['cnt']/cross_com['sum']
+
+#%%
+#計算推文帳號在每個作者下推文的機率
+grouped_vpv = data[data['推文類型'] == '推']
+grouped_2 = grouped_vpv.groupby(['推文作者', '作者'], as_index=False)['推文內文']
+com = grouped_2.agg({'cnt': 'count'})
+
+sum_com = com.groupby(['推文作者'], as_index=False)['cnt'].agg({'sum': 'sum'})
+cross_com = pd.merge(com, sum_com)
+cross_com['留言機率'] = cross_com['cnt']/cross_com['sum']
 
 #%%
