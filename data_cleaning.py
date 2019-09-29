78d184483e4e54392051c3db3cf1fd3057b2ff86
terrence03 <terrence0303@protonmail.com>
2019-09-29 14:55:40 +0800

    完成

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,11 +1,11 @@
-#%%
+# %%
 # 匯入套件
 import sqlite3
 import pandas as pd
 from tqdm import tqdm
 import re
 
-#%%
+# %%
 '''
 以下問題會在程式中處理
 1. 文章ID重複
@@ -23,29 +23,31 @@ import re
 
 # 匯入原始資料
 with sqlite3.connect(r'D:\\research\\data.db') as con:
-    df = pd.read_sql('SELECT * FROM 合併資料表', con=con).drop_duplicates('文章ID')  
+    df = pd.read_sql('SELECT * FROM 合併資料表', con=con).drop_duplicates('文章ID')
 # 順便刪除 1. 文章ID重複項
 print('匯入原始資料完成')
 
-#%%
+# %%
 # 處理 2.3.文章標題遺漏和發文時間遺漏
 # 2.3.的情況幾乎都會同時發生，所以以發文時間為目標
-clean_list1 = df.index[df['發文時間'] == '#N/A'].tolist()   #取得發文時間為#N/A的index
+clean_list1 = df.index[df['發文時間'] == '#N/A'].tolist()  # 取得發文時間為#N/A的index
 df.drop(clean_list1, inplace=True)
 print('刪除遺漏值完成')
 
-#%%
+# %%
 # 4.5.的問題一起處理
 # 規則是僅保留由英數字組成的帳號
-df.reset_index(drop=True, inplace=True)  # 重新排列index才能使用for迴圈。舊的index“不會”保留在首個colunm
+# 重新排列index才能使用for迴圈。舊的index“不會”保留在首個colunm
+df.reset_index(drop=True, inplace=True)
 print('重新排列index')
 
 df['作者'] = df['作者'].str.split(' ').str[0]
 print('處理作者資料完成')
 
-#%%
+# %%
 # 切分推文列表，將切分後的結果存入新的data中
-column_name1 = ['候選人', '關鍵字', '文章ID', '作者', '文章標題', '發文時間', '文章IP', '文章內文', '推文']
+column_name1 = ['候選人', '關鍵字', '文章ID', '作者',
+                '文章標題', '發文時間', '文章IP', '文章內文', '推文']
 resultlist1 = []
 
 for i in tqdm(range(df.shape[0])):
@@ -56,10 +58,10 @@ for i in tqdm(range(df.shape[0])):
     post_title = df.iloc[i, 4]
     post_time = df.iloc[i, 5]
     post_IP = df.iloc[i, 6]
-    #post_numofcom = df.iloc[i, 7]   #不再保留留言數的資料
+    # post_numofcom = df.iloc[i, 7]   #不再保留留言數的資料
     post_comment = df.iloc[i, 8]
     push_list = str(df.iloc[i, 9])
-    push_comment = push_list.split('\n') 
+    push_comment = push_list.split('\n')
 
     for j in range(len(push_comment)):
         if push_comment[j].startswith('推 '):
@@ -79,13 +81,13 @@ data = pd.DataFrame(resultlist1)
 data.columns = column_name1
 print('切分推文列表完成')
 
-#%%
+# %%
 # 根據測試，推文中有部分內容查無":"字元，這些推文會導致程式出錯，故先刪除
 clean_list2 = data.index[~ data['推文'].str.contains(':')].tolist()
 data.drop(clean_list2, inplace=True)
 data.reset_index(drop=True, inplace=True)
 
-#%%
+# %%
 # 切分每則貼文，將結果存入data_plus中
 # 最後將data_plus合併到data中
 column_name2 = ['推文類型', '推文作者', '推文內文', '推文IP', '推文時間']
@@ -133,17 +135,17 @@ for p in tqdm(range(data.shape[0])):
         push_time = str('2018/' + date + ' ' + time)
     else:
         push_time = str('2018/xx/xx' + ' ' + time)
-    
+
     result = [push_type, push_author, push_comment, push_IP, push_time]
     resultlist2.append(result)
 
 data_plus = pd.DataFrame(resultlist2)
 data_plus.columns = column_name2
-data = pd.concat([data, data_plus], axis=1) #axis=1是橫向合併，axis=0是縱向，預設是axis=0
+data = pd.concat([data, data_plus], axis=1)  # axis=1是橫向合併，axis=0是縱向，預設是axis=0
 del data['推文']
 print('切分推文內容完成')
 
-#%%
+# %%
 # 匯出到資料庫
 print('開始匯出到資料庫，請稍後')
 conn = sqlite3.connect(r'E:\\研究專案\\data.db')
--- 推文數計算.py
+++ 推文數計算.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import sqlite3
 import pandas as pd
 from tqdm import tqdm
@@ -14,15 +14,15 @@ count_list = []
 net_upvote = 0
 for i in tqdm(range(rows)):
     if data.iloc[i, 0] == data.iloc[i-1, 0]:
-        if data.iloc[i, 5] == "推" :
+        if data.iloc[i, 5] == "推":
             net_upvote = net_upvote + 1
-        elif data.iloc[i, 5] == "噓" :
+        elif data.iloc[i, 5] == "噓":
             net_upvote = net_upvote - 1
         elif data.iloc[i, 5] == "→":
             net_upvote = net_upvote
         count_list.append(net_upvote)
 
-        #print("同文章")
+        # print("同文章")
     else:
         net_upvote = 0
 
@@ -34,14 +34,16 @@ for i in tqdm(range(rows)):
             net_upvote = net_upvote
         count_list.append(net_upvote)
 
-        #print("不同文章")
+        # print("不同文章")
 data['淨推數'] = count_list
 
-#%%
+# %%
 # 計算留言帳號在每個作者下留言的機率
-count_comment = data.groupby(['推文作者', '作者', '推文類型'], as_index=False)['推文內文'].agg({'在此作者下的留言數': 'count'})
+count_comment = data.groupby(['推文作者', '作者', '推文類型'], as_index=False)[
+    '推文內文'].agg({'在此作者下的留言數': 'count'})
 
-sum_comment = count_comment.groupby(['推文作者'], as_index=False)['在此作者下的留言數'].agg({'總留言數': 'sum'})
+sum_comment = count_comment.groupby(['推文作者'], as_index=False)[
+    '在此作者下的留言數'].agg({'總留言數': 'sum'})
 
 cross_comment = pd.merge(count_comment, sum_comment)
 cross_comment['在此作者下的留言機率'] = cross_comment['在此作者下的留言數']/cross_comment['總留言數']
@@ -51,24 +53,28 @@ cross_comment['在此作者下的留言機率'] = cross_comment['在此作者下
 #cross_comment.drop(error_list, inplace=True)
 #cross_comment.reset_index(drop=True, inplace=True)
 
-#%%
+# %%
 # 計算推文帳號在每個作者下推文的機率
 data_upvote = data[data['推文類型'] == '推']
 
-count_upvote = data_upvote.groupby(['推文作者', '作者'], as_index=False)['推文類型'].agg({'在此作者下的推文數': 'count'})
+count_upvote = data_upvote.groupby(['推文作者', '作者'], as_index=False)[
+    '推文類型'].agg({'在此作者下的推文數': 'count'})
 
-sum_upvote = count_upvote.groupby(['推文作者'], as_index=False)['在此作者下的推文數'].agg({'總推文數': 'sum'})
+sum_upvote = count_upvote.groupby(['推文作者'], as_index=False)[
+    '在此作者下的推文數'].agg({'總推文數': 'sum'})
 
 cross_upvote = pd.merge(count_upvote, sum_upvote)
 cross_upvote['推文機率'] = cross_upvote['在此作者下的推文數']/cross_upvote['總推文數']
 
-#%%
+# %%
 # 計算推文帳號在每個作者下推文的機率
 data_dnvote = data[data['推文類型'] == '噓']
 
-count_dnvote = data_dnvote.groupby(['推文作者', '作者'], as_index=False)['推文類型'].agg({'在此作者下的噓文數': 'count'})
+count_dnvote = data_dnvote.groupby(['推文作者', '作者'], as_index=False)[
+    '推文類型'].agg({'在此作者下的噓文數': 'count'})
 
-sum_dnvote = count_dnvote.groupby(['推文作者'], as_index=False)['在此作者下的噓文數'].agg({'總噓文數': 'sum'})
+sum_dnvote = count_dnvote.groupby(['推文作者'], as_index=False)[
+    '在此作者下的噓文數'].agg({'總噓文數': 'sum'})
 
 cross_dnvote = pd.merge(count_dnvote, sum_dnvote)
 cross_dnvote['噓文機率'] = cross_dnvote['在此作者下的噓文數']/cross_dnvote['總噓文數']
@@ -78,13 +84,17 @@ error_list = cross_dnvote.index[cross_dnvote['推文作者'] == ''].tolist()
 cross_dnvote.drop(error_list, inplace=True)
 cross_dnvote.reset_index(drop=True, inplace=True)
 
-#%%
+# %%
 # 計算留言者的推文率，噓文率，箭頭率
-total_comment = data.groupby(['推文作者'], as_index=False)['推文內文'].agg({'總留言數': 'count'})  # 用來計算每個留言帳號在整個資料中的留言數量
-total_upvote = data_upvote.groupby(['推文作者'], as_index=False)['推文內文'].agg({'總推文數': 'count'})
-total_dnvote = data_dnvote.groupby(['推文作者'], as_index=False)['推文內文'].agg({'總噓文數': 'count'})
+total_comment = data.groupby(['推文作者'], as_index=False)['推文內文'].agg(
+    {'總留言數': 'count'})  # 用來計算每個留言帳號在整個資料中的留言數量
+total_upvote = data_upvote.groupby(['推文作者'], as_index=False)[
+    '推文內文'].agg({'總推文數': 'count'})
+total_dnvote = data_dnvote.groupby(['推文作者'], as_index=False)[
+    '推文內文'].agg({'總噓文數': 'count'})
 
-total_info = pd.merge(pd.merge(total_comment, total_upvote, how='left'), total_dnvote, how='left')
+total_info = pd.merge(pd.merge(total_comment, total_upvote,
+                               how='left'), total_dnvote, how='left')
 total_info.fillna(0, inplace=True)
 total_info['總箭頭數'] = total_info['總留言數']-total_info['總推文數']-total_info['總噓文數']
 total_info['推文率'] = total_info['總推文數']/total_info['總留言數']
@@ -95,7 +105,7 @@ total_info['總推文數'] = total_info['總推文數'].astype('int64')
 total_info['總噓文數'] = total_info['總噓文數'].astype('int64')
 total_info['總箭頭數'] = total_info['總箭頭數'].astype('int64')
 
-#%%
+# %%
 # 輸出結果
 with sqlite3.connect(r'E:\\research\\資料\\data.db') as con:
     data.to_sql(name='推文資料', con=con, if_exists='append', index=False)
