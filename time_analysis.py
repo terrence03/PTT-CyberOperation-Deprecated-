cab497c2479ecce75d030523ed8a598a084c7ac8
terrence03 <terrence0303@protonmail.com>
2019-11-23 01:11:56 +0800

    更新時間分析

--- time_analysis.py
+++ time_analysis.py
@@ -1,35 +1,16 @@
 # %%
+import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import sqlite3
 from datetime import datetime
 from tqdm import tqdm
 
+# 讀取資料
 with sqlite3.connect(r'E:\\research\\data\\sql\\data.db') as con:
     df = pd.read_sql('SELECT 推文作者,文章ID,推文時間 FROM 推文資料', con=con)
 
-
-def get_index(df, col_name, text):
-    result_list = df.index[df[col_name] == text].tolist()
-
-    return result_list
-
-
-def get_ChangeDate(time_col):
-    time_list = []
-    for i in tqdm(range(len(time_col))):
-        try:
-            time = datetime.strptime(time_col[i], '%Y/%m/%d %H:%M')
-        except:
-            time = 'N/A'
-
-        time_list.append(time)
-
-    return time_list
-# get_ChangeDate()用來將字串的日期格式轉為datetime的格式
-# 更好的方式應該用apply()，但原始資料中有些資料格式無法匹配，例如'2018/56/33 25:69'，所以改用迴圈的方式逐條轉化
-
-
+'''
 time_section = {0: '00~03',  1: '00~03',  2: '00~03',
                 3: '03~06',  4: '03~06',  5: '03~06',
                 6: '06~09',  7: '06~09',  8: '06~09',
@@ -47,43 +28,93 @@ worktime = {0: '0',  1: '0',  2: '0',
             15: '1', 16: '1', 17: '1',
             18: '0', 19: '0', 20: '0',
             21: '0', 22: '0', 23: '0', }
-
+'''
 # df['time'][0].time().hour
 
 # %%
-df.drop(get_index(df, '推文時間', '2018/xx/xx '), inplace=True)
+# 整理資料
+
+
+def get_index(df, col_name, text):
+    result_list = df.index[df[col_name] == text].tolist()
+
+    return result_list
+
+
+df.drop(get_index(df, '推文時間', '2018/xx/xx '), inplace=True)  # 先移除遺漏資料
 df.reset_index(drop=True, inplace=True)
 
-df['time'] = get_ChangeDate(df['推文時間'])
-df.drop(get_index(df, 'time', 'N/A'), inplace=True)
 
+def get_ChangeDate(time_col):
+    time_list = []
+    for i in tqdm(range(len(time_col))):
+        try:
+            time = datetime.strptime(time_col[i], '%Y/%m/%d %H:%M')
+        except:
+            time = 'N/A'
+
+        time_list.append(time)
+
+    return time_list
+# get_ChangeDate()用來將字串的日期格式轉為datetime的格式
+# 更好的方式應該用apply()，但原始資料中有些資料格式無法匹配，例如'2018/56/33 25:69'，所以改用迴圈的方式逐條轉化
+
+
+df['time'] = get_ChangeDate(df['推文時間'])  # 將字串轉為時間資料
+df.drop(get_index(df, 'time', 'N/A'), inplace=True)  # 移除無法被轉換的資料
+
+# 設定時間範圍
+start_date = datetime(2018, 6, 17)
+end_date = datetime(2018, 11, 24)
+mask = (df['time'] > start_date) & (df['time'] < end_date)
+df = df.loc[mask]
+df.reset_index(drop=True, inplace=True)
+
+
+# 將提取時間中的星期和鐘點
 df['weekday'] = df['time'].apply(lambda x: x.isoweekday())
-# isoweekday()可以回傳該日期是星期幾，星期一回傳 1 ，星期天回傳 7
+df['worktime'] = df['time'].apply(lambda x: x.hour)
 
-#df['時間段'] = df['time'].apply(lambda x: time_section[x.time().hour])
-df['worktime'] = df['time'].apply(lambda x: worktime[x.time().hour])
-
-
-#df = df.set_index(df['time'])
 
 # %%
-df.reset_index(drop=True, inplace=True)
-df.columns = ['pushAuthor', 'postID', 'pushtime',
-              'time', 'weekday', 'worktime']
+# 統計帳號留言時間分佈
+weekday_cnt = df[['weekday', 'worktime']].groupby(
+    ['weekday'], as_index=False).count()
+weekday_cnt.rename(columns={'worktime': 'count'}, inplace=True)
 
-df_le = df[['pushAuthor', 'postID', 'weekday', 'worktime']]
-#dummy_timerange = pd.get_dummies(df_le['timerange'])
-#dummy_weekday = pd.get_dummies(df_le['weekday'])
-df_le = pd.get_dummies(df_le, columns=['worktime'])
+worktime_cnt = df[['weekday', 'worktime']].groupby(
+    ['worktime'], as_index=False).count()
+worktime_cnt.rename(columns={'weekday': 'count'}, inplace=True)
 
 # %%
-df_ts = df[['pushAuthor', 'time']]
-#df_ts['ts'] = df_ts['time'].apply(lambda x: x.time())
-df_ts.set_index(df_ts['time'], inplace=True)
+# 繪圖顯示留言時間分佈
+plt.figure(figsize=(10, 12))
 
-with sqlite3.connect(r'E:\\research\\data\\sql\\data.db') as con:
-    df_ts.to_sql('推文時間序列', index=False, con=con)
+# 上線星期
+plt.subplot(2, 1, 1)
 
-#ts_count = df_ts['pushAuthor'].resample('H', kind='timestamp').count()
-# ts_count.plot()
+x_weekday = weekday_cnt['weekday']
+y_weekday = weekday_cnt['count']
+plt.plot(x_weekday, y_weekday, 'o-', label='count')
+plt.xticks(range(1, 8))
+plt.xlabel('Weekday')
+plt.ylabel('Frequency')
+plt.grid(True, linestyle="--", color='gray', linewidth='0.5', axis='both')
+plt.title('Weekday')
+
+# 上線時間
+plt.subplot(2, 1, 2)
+
+x_worktime = worktime_cnt['worktime']
+y_worktime = worktime_cnt['count']
+
+plt.plot(x_worktime, y_worktime, 'o-', label='count')
+plt.xticks(range(24))
+plt.xlabel('Time of Day')
+plt.ylabel('Frequency')
+plt.grid(True, linestyle="--", color='gray', linewidth='0.5', axis='both')
+plt.title('Worktime')
+
+plt.show()
+
 # %%
